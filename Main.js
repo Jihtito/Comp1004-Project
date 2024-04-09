6685f35f108dbfab85b4d185b1ca968a516d7376
Jim Toogood <jimtoogood04@gmail.com>
2024-04-09 13:21:30 +0100

    Added Health, Levelling, Damage, Attacking and better Enemy Player Tracking
    Added a level and xp system to the player that scales their stats (health and attack) with their level. Experience is gained from killing enemies which is now possible by attacking them (using the space bar). The damage of this attack scales with the player's attack stat.
    
    Added invincibility frames to both the player and enemy in order to stop them dying too quickly.
    
    Improved enemy tracking the player and moving towards them, additionally added an element of randomness to the enemy's movements, so that when multiple enemies are on screen they don't all move at the same time.

--- Main.js
+++ Main.js
@@ -8,8 +8,8 @@ class Game {
         
         this.player = new Player();
         this.entity_list = [this.player];
-        for (let i = 0; i < 3; i++) {
-            this.entity_list.push(new Zombie());
+        for (let i = 0; i < 5; i++) {
+            this.entity_list.push(new Zombie(this.player.level));
         }
 
         this.WIN.fillStyle = BLACK; this.WIN.fillRect(0, 0, WIDTH, HEIGHT);
@@ -17,8 +17,27 @@ class Game {
 
     update() {
         for (let i = 0; i < this.entity_list.length; i++) {
-            this.entity_list[i].update(this.frame_counter, this.player.x, this.player.y);
+            let hit = this.entity_list[i].update(this.frame_counter, this.player.x, this.player.y);
+
+            if (this.entity_list[i].is_dead) {
+                this.player.xp += +this.entity_list[i].max_health;
+                this.entity_list[i] = null; this.entity_list.splice(i, 1);
+                continue;
+            }
+
+            if (hit) {
+                if (this.player.invincibility_frames == 0) {
+                    this.player.health -= this.entity_list[i].attack;
+                    this.player.invincibility_frames = 35;
+                }
+            }
         }
+
+        if (this.player.projectile != null) {
+            this.player.projectile.update(this.entity_list);
+        }
+
+        if (this.player.health <= 0) { document.write("You Died."); throw "You Died."; }
     }
 
     draw() {
@@ -27,6 +46,10 @@ class Game {
         
         for (let i = 0; i < this.entity_list.length; i++) {
             this.entity_list[i].draw(this.WIN);
+        }
+
+        if (this.player.projectile != null) {
+            this.player.projectile.draw(this.WIN);
         }
     }
 
--- Settings.js
+++ Settings.js
@@ -6,8 +6,14 @@ const MAP_WIDTH = 350; const MAP_HEIGHT = 223;
 
 const FRAME_WIDTH = 25; const FRAME_HEIGHT = 25;
 const ZOMBIE_FRAME_WIDTH = 32; const ZOMBIE_FRAME_HEIGHT = 32;
+const PROJECTILE_FRAME_WIDTH = 32; const PROJECTILE_FRAME_HEIGHT = 32;
 
 const BLACK = "rgb(0, 0, 0)";       // Defining colors with RGB values
+const GRAY = "rgb(50, 50, 50)";
+const RED = "rgb(255, 0, 0)";
+const YELLOW = "rgb(255, 255, 0)";
+const GREEN = "rgb(0, 255, 0)";
+const CYAN = "rgb(0, 255, 255)";
 const SF = 3.5;                     // Defining scale factor of pixel images
 
 const PLAYER_IMAGE = new Image(FRAME_WIDTH * 11 * SF, FRAME_HEIGHT * 4 * SF);
@@ -15,6 +21,9 @@ PLAYER_IMAGE.src = "Files/sprite_sheet.png";
 
 const ZOMBIE_IMAGE = new Image(ZOMBIE_FRAME_WIDTH * 9 * SF, ZOMBIE_FRAME_HEIGHT * 4 * SF);
 ZOMBIE_IMAGE.src = "Files/zombie.png";
+
+const PROJECTILE_IMAGE = new Image(PROJECTILE_FRAME_WIDTH * SF, PROJECTILE_FRAME_HEIGHT * 4 * SF);
+PROJECTILE_IMAGE.src = "Files/projectile.png";
 
 const MAP_IMAGE = new Image(MAP_WIDTH * SF, MAP_HEIGHT * SF);
 MAP_IMAGE.src = "Files/background.png";
